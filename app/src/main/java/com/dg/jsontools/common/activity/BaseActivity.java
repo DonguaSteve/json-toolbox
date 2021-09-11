579953bb9dbb2dd60bffb77fc910cc7eb4750b65
刘天然 <9304691+liu-natural@user.noreply.gitee.com>
2021-09-11 11:37:18 +0000

    <null>

---
+++ app/src/main/java/com/dg/jsontools/common/activity/BaseActivity.java
@@ -0,0 +1,39 @@
+package com.dg.jsontools.common.activity;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.MenuItem;
+import com.dg.jsontools.App;
+import com.dg.jsontools.R;
+import com.dg.jsontools.util.UiUtils;
+
+public abstract class BaseActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initStatusBar();
+        initActionBar();
+    }
+
+    protected void initStatusBar() {
+        UiUtils.setStatusBarLightMode(this, getResources().getBoolean(R.bool.light_status_bar));
+    }
+
+    protected void initActionBar() {
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(canBack());
+        }
+    }
+
+    public boolean canBack() {
+        return false;
+    }
+
+    public App getApp() {
+        return App.getApp();
+    }
+}
